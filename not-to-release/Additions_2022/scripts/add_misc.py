3f5846d15e244928c507f78508e923d897a44d2e
hinrikur <hinrik.hafst@gmail.com>
2022-10-25 17:08:36 +0000

    metadata validation fixes for additions

--- not-to-release/Additions_2022/scripts/add_misc.py
+++ not-to-release/Additions_2022/scripts/add_misc.py
@@ -1,7 +1,5 @@
 import pyconll
 
-from features import Features, ICE_Features
-
 # a function to write the output to a file
 def write_to_conllu(conllu_file, conll):
     with open(conllu_file, 'w', encoding='utf-8') as f:    
@@ -16,22 +14,36 @@ def make_sets(d):
         # print(k,v)
         if v:
             d[k] = set((v,))
+        
     return d
 
 # a function to add info to a misc column in a conllu file token
-def add_tag_to_misc(token, tag, sentence):
-    misc = token.misc
-    misc['IFD_tag'] = set((tag,))
+def make_misc_column(token, sentence):
+    NO_SPACE_AFTER = "„-/("
+    NO_SPACE_BEFORE = "-%/,.“”):?!"
+    misc = {}
+    
+    # check for known tokens that should not have a space before
+    if len(sentence) != int(token.id) and sentence[str(int(token.id)+1)].form in NO_SPACE_BEFORE:
+        misc['SpaceAfter'] = set(('No',))
+    # check for known tokens that should not have a space after
+    if token.form in NO_SPACE_AFTER:
+        misc['SpaceAfter'] = set(('No',))
+    if token.form == '-' and ' - ' in sentence.text:
+        misc = {}
+    if len(sentence) != int(token.id) and sentence[str(int(token.id)+1)].form == '-' and ' - ' in sentence.text:
+        misc = {}
     return filter_none_values(misc)
 
 # a function to filter out None values in a dictionary
 def filter_none_values(d):
     return {k: v for k, v in d.items() if v is not None}
 
+
 if __name__ == '__main__':
     
     # load the conllu file
-    conll = pyconll.load_from_file("additions-with-misc.conllu")
+    conll = pyconll.load_from_file("additions-with-metadata.conllu")
 
     sent_num = len(conll)
     runner = 0
@@ -40,19 +52,8 @@ if __name__ == '__main__':
         runner += 1
         # the Features module used to tag the sentence as a whole 
         # (Note: ABLTagger 2018 is used to tag the sentences)
-        tag_dict = Features.tagged_sent(sentence.text)
         for token in sentence:
-            ifd_tag = tag_dict.get(token.form, "x")[0]
-            # the ICE_Features module used when ABLTagger 2018 is not able to tag a token
-            if ifd_tag == "x":
-                feats = ICE_Features(token.xpos).get_features()
-            else:
-                feats = Features(ifd_tag).features
-            # overwrite the original features
-            token.feats = filter_none_values(make_sets(dict(feats)))
-            # overwrite the original misc column
-            token.misc = add_tag_to_misc(token, ifd_tag, sentence)
-            # Note: moved to another script
+            token.misc = make_misc_column(token, sentence)
             
         #     print(token.id, token.form, token.lemma, token.upos, token.xpos, token.feats, token.head, token.deprel, token.deps, token.misc)
         # input()
@@ -61,4 +62,4 @@ if __name__ == '__main__':
         print(f"Processed sentence {runner} of {sent_num}", end='\r')
     
     # write the new conllu file
-    write_to_conllu("additions-with-features.conllu", conll)
+    write_to_conllu("additions-with-misc.conllu", conll)
